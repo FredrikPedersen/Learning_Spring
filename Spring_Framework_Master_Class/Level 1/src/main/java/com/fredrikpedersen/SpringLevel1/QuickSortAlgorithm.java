f6e894920df54fa17cc24c4c666bcefe076506a9
FredrikPedersen <fredrikhp@gmail.com>
2020-01-10 10:04:01 +0100

    Completed Section 4: Part 11: Dynamic auto wiring and Troubleshooting - @Primary

--- Spring_Framework_Master_Class/Level 1/src/main/java/com/fredrikpedersen/SpringLevel1/QuickSortAlgorithm.java
+++ Spring_Framework_Master_Class/Level 1/src/main/java/com/fredrikpedersen/SpringLevel1/QuickSortAlgorithm.java
@@ -1,7 +1,10 @@
 package com.fredrikpedersen.SpringLevel1;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm{
 
     //Dummy method for demonstration purposes
