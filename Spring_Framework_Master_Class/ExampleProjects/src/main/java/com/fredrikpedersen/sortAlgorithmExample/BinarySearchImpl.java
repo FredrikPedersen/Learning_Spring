5e58e4c02cd1921d71e60c8de4ac24f56b5194ec
FredrikPedersen <fredrikhp@gmail.com>
2020-01-12 18:01:58 +0100

    Completed Section 5: Part 25: Lifecycle of a Bean - @PostConstruct and @PreDestroy

--- Spring_Framework_Master_Class/ExampleProjects/src/main/java/com/fredrikpedersen/sortAlgorithmExample/BinarySearchImpl.java
+++ Spring_Framework_Master_Class/ExampleProjects/src/main/java/com/fredrikpedersen/sortAlgorithmExample/BinarySearchImpl.java
@@ -1,14 +1,21 @@
 package com.fredrikpedersen.sortAlgorithmExample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //Use prototype to get different instances when creating more than one bean, use singleton to get only one instance
 public class BinarySearchImpl {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("bubble")
@@ -23,5 +30,15 @@ public class BinarySearchImpl {
         sortAlgorithm.sort(numbers);
 
         return 3;
+    }
+
+    @PostConstruct //Called once the bean has been instantiated
+    public void postConstruct() {
+        logger.info("postConstruct: called");
+    }
+
+    @PreDestroy //Called right before the bean is destroyed
+    public void preDestroy() {
+        logger.info("preDestroy: called");
     }
 }
