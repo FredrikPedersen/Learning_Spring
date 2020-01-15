2dab3d6a6a67cc15e70b122b3d32aea17ad44bb3
FredrikPedersen <fredrikhp@gmail.com>
2020-01-15 11:10:34 +0100

    Completed Section 3: Part 34: Create an Spring Framework DI Example Project

---
+++ Spring_Framework_Beginner_To_Guru/dependencyinjection-demo/src/main/java/com/fredrikpedersen/dependencyinjectiondemo/DependencyinjectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fredrikpedersen.dependencyinjectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyinjectionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyinjectionDemoApplication.class, args);
+    }
+
+}
---
+++ Spring_Framework_Beginner_To_Guru/dependencyinjection-demo/src/test/java/com/fredrikpedersen/dependencyinjectiondemo/DependencyinjectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredrikpedersen.dependencyinjectiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyinjectionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
