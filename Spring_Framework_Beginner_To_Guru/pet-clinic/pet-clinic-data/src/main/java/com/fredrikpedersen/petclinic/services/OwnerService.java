264abdc0be3e20daf5ad871bd566fee5b7139ea0
FredrikPedersen <fredrikhp@gmail.com>
2020-01-16 14:20:01 +0100

    Completed Section 4: Part 68:  Spring Pet Clinic - Refactor Services to Common Interface

--- Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/services/OwnerService.java
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/services/OwnerService.java
@@ -2,17 +2,15 @@ package com.fredrikpedersen.petclinic.services;
 
 import com.fredrikpedersen.petclinic.model.people.Owner;
 
-import java.util.Set;
-
 /**
  * @author Fredrik Pedersen
  * @version 1.0
  * @since 16/01/2020 at 13:21
  */
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
+
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 }
+
--- Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/services/VeterinaryService.java
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/services/VeterinaryService.java
@@ -2,15 +2,11 @@ package com.fredrikpedersen.petclinic.services;
 
 import com.fredrikpedersen.petclinic.model.people.Veterinary;
 
-import java.util.Set;
-
 /**
  * @author Fredrik Pedersen
  * @version 1.0
  * @since 16/01/2020 at 13:24
  */
-public interface VeterinaryService {
-    Veterinary findById(Long id);
-    Veterinary save(Veterinary veterinary);
-    Set<Veterinary> findAll();
+
+public interface VeterinaryService extends CrudService<Veterinary, Long> {
 }
