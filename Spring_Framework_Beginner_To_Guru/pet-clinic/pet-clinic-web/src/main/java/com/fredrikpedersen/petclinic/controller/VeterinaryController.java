75d4c42a33cb8b6504b32f0df6017edd353b087f
FredrikPedersen <fredrikhp@gmail.com>
2020-01-21 16:26:39 +0100

    Completed Section 5: Part 90: Spring Pet Clinic - List Vets. Closes #13.

--- Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-web/src/main/java/com/fredrikpedersen/petclinic/controller/VeterinaryController.java
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-web/src/main/java/com/fredrikpedersen/petclinic/controller/VeterinaryController.java
@@ -1,6 +1,8 @@
 package com.fredrikpedersen.petclinic.controller;
 
+import com.fredrikpedersen.petclinic.services.VeterinaryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,9 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class VeterinaryController {
 
     private static final String VETERINARIES_VIEW = "veterinaries";
+    private static final String ALL_VETERINARIES_ATTRIBUTE = "veterinaries";
+    private final VeterinaryService veterinaryService;
+
+    public VeterinaryController(VeterinaryService veterinaryService) {
+        this.veterinaryService = veterinaryService;
+    }
 
     @RequestMapping({VETERINARIES_VIEW, VETERINARIES_VIEW + ".html"})
-    public String listVeterinaries() {
+    public String listVeterinaries(Model model) {
+
+        model.addAttribute(ALL_VETERINARIES_ATTRIBUTE, veterinaryService.findAll());
+
         return VETERINARIES_VIEW;
     }
 }
