01f4bf46f7c6653d21dc8ccc57f381d0e9f5f6ec
FredrikPedersen <fredrikhp@gmail.com>
2020-01-22 12:04:29 +0100

    Completed Section 7: Part 107:  Create Index Page

---
+++ Spring_Framework_Beginner_To_Guru/recipe-project/src/main/java/com/fredrikpedersen/recipeproject/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package com.fredrikpedersen.recipeproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Fredrik Pedersen
+ * @version 1.0
+ * @since 22/01/2020 at 11:58
+ */
+
+@Controller
+public class IndexController {
+
+    private final String INDEX_VIEW = "index";
+
+    @RequestMapping({"", "/", INDEX_VIEW})
+    public String getIndexPage() {
+        return INDEX_VIEW;
+    }
+}
