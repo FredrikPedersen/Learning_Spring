eb674144c0bcb57326b0b21244124aa74bd3c29f
FredrikPedersen <fredrikhp@gmail.com>
2020-01-23 12:43:31 +0100

    Completed Section 7: Part 129: Spring Pet Clinic - Pet Type Data on Startup. Closes #31.

--- Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-web/src/main/java/com/fredrikpedersen/petclinic/bootstrap/DataLoader.java
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-web/src/main/java/com/fredrikpedersen/petclinic/bootstrap/DataLoader.java
@@ -2,7 +2,9 @@ package com.fredrikpedersen.petclinic.bootstrap;
 
 import com.fredrikpedersen.petclinic.model.people.owners.Owner;
 import com.fredrikpedersen.petclinic.model.people.veterinaries.Veterinary;
+import com.fredrikpedersen.petclinic.model.pets.PetType;
 import com.fredrikpedersen.petclinic.services.OwnerService;
+import com.fredrikpedersen.petclinic.services.PetTypeService;
 import com.fredrikpedersen.petclinic.services.VeterinaryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,37 +20,58 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VeterinaryService veterinaryService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VeterinaryService veterinaryService) {
+    public DataLoader(OwnerService ownerService, VeterinaryService veterinaryService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.veterinaryService = veterinaryService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Owner owner1 = new Owner();
-        owner1.setFirstName("Martina");
-        owner1.setLastName("Førre");
-        ownerService.save(owner1);
-
-        Owner owner2 = new Owner();
-        owner2.setFirstName("Signe");
-        owner2.setLastName("Eide");
-        ownerService.save(owner2);
-
+        createOwners();
         System.out.println("Created owner data...");
 
-        Veterinary veterinary1 = new Veterinary();
-        veterinary1.setFirstName("Victor");
-        veterinary1.setLastName("Pishva");
-        veterinaryService.save(veterinary1);
-
-        Veterinary veterinary2 = new Veterinary();
-        veterinary2.setFirstName("Nikita");
-        veterinary2.setLastName("Petrovs");
-        veterinaryService.save(veterinary2);
-
+        createVeterinaries();
         System.out.println("Created veterinary data...");
 
+        createPetTypes();
+        System.out.println("Created pet type data...");
+
+    }
+
+    private void createOwners() {
+        Owner owner = new Owner();
+        owner.setFirstName("Martina");
+        owner.setLastName("Førre");
+        ownerService.save(owner);
+
+        owner = new Owner();
+        owner.setFirstName("Signe");
+        owner.setLastName("Eide");
+        ownerService.save(owner);
+    }
+
+    private void createVeterinaries() {
+        Veterinary veterinary = new Veterinary();
+        veterinary.setFirstName("Victor");
+        veterinary.setLastName("Pishva");
+        veterinaryService.save(veterinary);
+
+        veterinary = new Veterinary();
+        veterinary.setFirstName("Nikita");
+        veterinary.setLastName("Petrovs");
+        veterinaryService.save(veterinary);
+    }
+
+    private void createPetTypes() {
+        PetType petType = new PetType();
+        petType.setName("Dog");
+        petTypeService.save(petType);
+
+        petType = new PetType();
+        petType.setName("Cat");
+        petTypeService.save(petType);
     }
 }
