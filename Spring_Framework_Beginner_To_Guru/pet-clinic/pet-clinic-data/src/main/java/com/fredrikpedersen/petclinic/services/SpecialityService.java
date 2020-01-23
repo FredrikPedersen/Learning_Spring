2e0ea10ec10b19c791b5aa01814d20cfc1a8ec0d
FredrikPedersen <fredrikhp@gmail.com>
2020-01-23 13:30:38 +0100

    Completed Section 7: Part 131: Spring Pet Clinic - Create Specialty Map Service. Closes #30.

---
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/services/SpecialityService.java
@@ -0,0 +1,11 @@
+package com.fredrikpedersen.petclinic.services;
+
+import com.fredrikpedersen.petclinic.model.people.veterinaries.Speciality;
+
+/**
+ * @author Fredrik Pedersen
+ * @version 1.0
+ * @since 23/01/2020 at 13:26
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,43 @@
+package com.fredrikpedersen.petclinic.services.map;
+
+import com.fredrikpedersen.petclinic.model.people.veterinaries.Speciality;
+import com.fredrikpedersen.petclinic.model.people.veterinaries.Veterinary;
+import com.fredrikpedersen.petclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author Fredrik Pedersen
+ * @version 1.0
+ * @since 23/01/2020 at 13:27
+ */
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
