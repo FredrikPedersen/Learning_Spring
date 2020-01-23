bd970bd45989586d28310fa81479ea4287c22ace
FredrikPedersen <fredrikhp@gmail.com>
2020-01-23 17:52:27 +0100

    Completed Section 8: Part 145: Creating Spring Data Repositories

---
+++ Spring_Framework_Beginner_To_Guru/recipe-project/src/main/java/com/fredrikpedersen/recipeproject/repositories/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.fredrikpedersen.recipeproject.repositories;
+
+import com.fredrikpedersen.recipeproject.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Fredrik Pedersen
+ * @version 1.0
+ * @since 23/01/2020 at 17:51
+ */
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+    
+}
---
+++ Spring_Framework_Beginner_To_Guru/recipe-project/src/main/java/com/fredrikpedersen/recipeproject/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,14 @@
+package com.fredrikpedersen.recipeproject.repositories;
+
+import com.fredrikpedersen.recipeproject.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Fredrik Pedersen
+ * @version 1.0
+ * @since 23/01/2020 at 17:51
+ */
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+}
