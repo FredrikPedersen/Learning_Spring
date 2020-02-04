335fec2f8e1a8db9484a596cc94cfa6846fcd4bf
Fredrik Pedersen <fredrikhp@gmail.com>
2020-02-04 15:29:00 +0100

    Completed Section 8: Part 156: Spring Pet Clinic - Add Map Based Visit Service. Closes #45.

---
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,54 @@
+package com.fredrikpedersen.petclinic.services.map;
+
+import com.fredrikpedersen.petclinic.model.Visit;
+import com.fredrikpedersen.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author Fredrik Pedersen
+ * @version 1.0
+ * @since 04.02.2020 at 15:20
+ */
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null ||
+            visit.getPet().getOwner().getId() == null)  {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
