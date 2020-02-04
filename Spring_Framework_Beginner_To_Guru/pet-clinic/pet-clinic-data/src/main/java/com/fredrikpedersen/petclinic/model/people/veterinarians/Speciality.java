8f9ae30b3d9d870d86adf25b3c8e52dbbba064e4
Fredrik Pedersen <fredrikhp@gmail.com>
2020-02-04 13:59:07 +0100

    Completed Section 8: Part 147: Spring Pet Clinic - Convert Vets to JPA Entities. Closes #36.

--- Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/model/people/veterinarians/Speciality.java
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/model/people/veterinarians/Speciality.java
@@ -2,13 +2,21 @@ package com.fredrikpedersen.petclinic.model.people.veterinarians;
 
 import com.fredrikpedersen.petclinic.model.BaseEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author Fredrik Pedersen
  * @version 1.0
  * @since 23/01/2020 at 12:22
  */
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
