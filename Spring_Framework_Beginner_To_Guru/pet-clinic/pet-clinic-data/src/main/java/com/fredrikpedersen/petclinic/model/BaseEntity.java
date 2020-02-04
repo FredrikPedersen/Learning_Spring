97dad49c13b2f5b5b1f8216873adcd5a1ebd2970
Fredrik Pedersen <fredrikhp@gmail.com>
2020-02-04 13:03:30 +0100

    Added Annotations to BaseEntity

--- Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/model/BaseEntity.java
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/model/BaseEntity.java
@@ -1,5 +1,9 @@
 package com.fredrikpedersen.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
@@ -8,8 +12,11 @@ import java.io.Serializable;
  * @since 16/01/2020 at 13:31
  */
 
+@MappedSuperclass
 public abstract class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -19,4 +26,5 @@ public abstract class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+    
 }
