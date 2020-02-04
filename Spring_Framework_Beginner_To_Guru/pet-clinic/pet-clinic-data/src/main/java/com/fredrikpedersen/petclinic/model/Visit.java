2d36db9f84d8632b586d8dcd6d49a071dfb53c69
Fredrik Pedersen <fredrikhp@gmail.com>
2020-02-04 14:17:39 +0100

    Completed Section 8: Part 148: Spring Pet Clinic - Create Visit Entity. Closes #37.

--- Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/model/Visit.java
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/model/Visit.java
@@ -2,6 +2,7 @@ package com.fredrikpedersen.petclinic.model;
 
 import com.fredrikpedersen.petclinic.model.pets.Pet;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
@@ -10,10 +11,18 @@ import java.time.LocalDate;
  * @since 23/01/2020 at 12:17
  */
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
--- Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/model/pets/Pet.java
+++ Spring_Framework_Beginner_To_Guru/pet-clinic/pet-clinic-data/src/main/java/com/fredrikpedersen/petclinic/model/pets/Pet.java
@@ -1,10 +1,13 @@
 package com.fredrikpedersen.petclinic.model.pets;
 
 import com.fredrikpedersen.petclinic.model.BaseEntity;
+import com.fredrikpedersen.petclinic.model.Visit;
 import com.fredrikpedersen.petclinic.model.people.owners.Owner;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Fredrik Pedersen
@@ -29,6 +32,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -60,5 +66,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
