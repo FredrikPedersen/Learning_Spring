feeb9da64e9035550bf181083b19a050d2fe16dc
FredrikPedersen <fredrikhp@gmail.com>
2020-02-05 20:41:19 +0100

    Completed Section 10: Part 173:  Using Mockito Mocks

---
+++ Spring_Framework_Beginner_To_Guru/recipe-project/src/test/java/com/fredrikpedersen/recipeproject/services/RecipeServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.fredrikpedersen.recipeproject.services;
+
+import com.fredrikpedersen.recipeproject.domain.Recipe;
+import com.fredrikpedersen.recipeproject.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Fredrik Pedersen
+ * @version 1.0
+ * @since 05/02/2020 at 20:34
+ */
+class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
