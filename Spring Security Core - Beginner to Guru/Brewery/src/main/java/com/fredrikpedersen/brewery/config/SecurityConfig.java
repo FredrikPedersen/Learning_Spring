65f6de10819fd1a7b7f0213b2be2c34284115a9a
Fredrik Pedersen <fredrikhp@gmail.com>
2021-05-03 11:52:44 +0200

    Completed Section 4: Part 26: Spring MVC Path Matchers

--- Spring Security Core - Beginner to Guru/Brewery/src/main/java/com/fredrikpedersen/brewery/config/SecurityConfig.java
+++ Spring Security Core - Beginner to Guru/Brewery/src/main/java/com/fredrikpedersen/brewery/config/SecurityConfig.java
@@ -17,7 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 .authorizeRequests(authorize -> authorize
                         .antMatchers(nonSecuredMvcPaths).permitAll()
-                        .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll())
+                        .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                        .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll())
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
