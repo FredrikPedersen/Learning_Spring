c08533b740ef001c79ba97995eab441fe3a56162
Fredrik Pedersen <fredrikhp@gmail.com>
2021-05-04 19:16:12 +0200

    Completed Section 6: Part 34: NoOp Password Encoder

--- Spring Security Core - Beginner to Guru/Brewery/src/main/java/com/fredrikpedersen/brewery/config/SecurityConfig.java
+++ Spring Security Core - Beginner to Guru/Brewery/src/main/java/com/fredrikpedersen/brewery/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.fredrikpedersen.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final String[] nonSecuredMvcPaths = new String[]{"/", "/webjars/**", "/login", "/resources/**", "/beers/find", "/beers*"};
+
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -30,14 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder authenticationManager) throws Exception {
-        authenticationManager.inMemoryAuthentication()
-                .withUser("admin")
-                .password("{noop}pw")
-                .roles("ADMIN")
-                .and()
-                .withUser("user")
-                .password("{noop}pw")
-                .roles("USER");
+        authenticationManager.inMemoryAuthentication().withUser("admin").password("pw").roles("ADMIN");
+        authenticationManager.inMemoryAuthentication().withUser("user").password("pw").roles("USER");
     }
 
     //Other method for configuring in-memory userDetails
