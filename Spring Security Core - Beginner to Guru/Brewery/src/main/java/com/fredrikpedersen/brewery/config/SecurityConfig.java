bd8839d4e1da3f65faaa6fd0649b8631ee312631
Fredrik Pedersen <fredrikhp@gmail.com>
2021-05-11 11:20:41 +0200

    Completed Section 7: Part 44: Spring Security Configuration

--- Spring Security Core - Beginner to Guru/Brewery/src/main/java/com/fredrikpedersen/brewery/config/SecurityConfig.java
+++ Spring Security Core - Beginner to Guru/Brewery/src/main/java/com/fredrikpedersen/brewery/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.fredrikpedersen.brewery.config;
 
 import com.fredrikpedersen.brewery.security.DomainPasswordEncoderFactories;
+import com.fredrikpedersen.brewery.security.RestHeaderAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -16,13 +20,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final String[] nonSecuredMvcPaths = new String[]{"/", "/webjars/**", "/login", "/resources/**", "/beers/find", "/beers*"};
 
-    @Bean
-    protected PasswordEncoder passwordEncoder() {
-        return DomainPasswordEncoderFactories.createDelegatingPasswordEncoder(); //Default is BCrypt
+    public RestHeaderAuthFilter restHeaderAuthFilter(final AuthenticationManager authenticationManager) {
+        final RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+        filter.setAuthenticationManager(authenticationManager);
+
+        return filter;
     }
 
     @Override
     protected void configure(final HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.addFilterBefore(restHeaderAuthFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class); //Adds restHeaderAuthFilter to execute before the UPAFilter in the filter chain.
+
         httpSecurity
                 .authorizeRequests(authorize -> authorize
                         .antMatchers(nonSecuredMvcPaths).permitAll()
@@ -34,6 +42,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return DomainPasswordEncoderFactories.createDelegatingPasswordEncoder(); //Default is BCrypt
     }
 
     @Override
--- Spring Security Core - Beginner to Guru/Brewery/src/main/java/com/fredrikpedersen/brewery/security/RestHeaderAuthFilter.java
+++ Spring Security Core - Beginner to Guru/Brewery/src/main/java/com/fredrikpedersen/brewery/security/RestHeaderAuthFilter.java
@@ -1,10 +1,12 @@
 package com.fredrikpedersen.brewery.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.servlet.ServletException;
