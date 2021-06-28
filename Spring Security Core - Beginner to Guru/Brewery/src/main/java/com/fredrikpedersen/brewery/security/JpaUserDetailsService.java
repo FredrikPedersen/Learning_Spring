31a4789eadea950305031f1794371134b31c56a2
Fredrik Pedersen <fredrikhp@gmail.com>
2021-06-28 10:12:33 +0200

    Completed Section 8: Part 54: User Details Service

---
+++ Spring Security Core - Beginner to Guru/Brewery/src/main/java/com/fredrikpedersen/brewery/security/JpaUserDetailsService.java
@@ -0,0 +1,59 @@
+package com.fredrikpedersen.brewery.security;
+
+import com.fredrikpedersen.brewery.domain.security.Authority;
+import com.fredrikpedersen.brewery.domain.security.User;
+import com.fredrikpedersen.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author Fredrik Pedersen
+ * @version 1.0
+ * @since 28/06/2021 at 10:04
+ */
+
+@Service
+@RequiredArgsConstructor
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        final User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username: " + username + " not found"));
+        return convertToSpringUser(user);
+    }
+
+    private org.springframework.security.core.userdetails.User convertToSpringUser(final User user) {
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getEnabled(),
+                user.getAccountNonExpired(),
+                user.getCredentialsNonExpired(),
+                user.getAccountNonLocked(),
+                convertToSpringAuthorities(user.getAuthorities())
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(final Set<Authority> authorities) {
+        if (authorities != null && authorities.size() > 0) {
+            return authorities.stream()
+                    .map(Authority::getRole)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+        } else {
+            return new HashSet<>();
+        }
+    }
+}
