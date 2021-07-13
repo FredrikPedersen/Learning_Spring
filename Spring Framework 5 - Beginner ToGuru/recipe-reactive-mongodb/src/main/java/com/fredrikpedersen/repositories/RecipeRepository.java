ff1130babd410bf07aa30b7e33779c26bc2f5457
Fredrik Pedersen <fredrikhp@gmail.com>
2021-07-13 17:39:21 +0200

    Added Section 21 base project

---
+++ Spring Framework 5 - Beginner ToGuru/recipe-reactive-mongodb/src/main/java/com/fredrikpedersen/repositories/RecipeRepository.java
@@ -0,0 +1,10 @@
+package com.fredrikpedersen.repositories;
+
+import com.fredrikpedersen.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeRepository extends CrudRepository<Recipe, String> {
+}
---
+++ Spring Framework 5 - Beginner ToGuru/recipe-reactive-mongodb/src/main/java/com/fredrikpedersen/services/UnitOfMeasureService.java
@@ -0,0 +1,13 @@
+package com.fredrikpedersen.services;
+
+import com.fredrikpedersen.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/28/17.
+ */
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
