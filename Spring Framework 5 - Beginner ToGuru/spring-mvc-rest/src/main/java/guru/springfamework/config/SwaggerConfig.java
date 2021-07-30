b876560a3dcad791d19aa015b349f601384f7430
Fredrik Pedersen <fredrikhp@gmail.com>
2021-07-30 09:53:14 +0200

    Completed Section 25: Part 400: Customizing Swagger with Meta Data

--- Spring Framework 5 - Beginner ToGuru/spring-mvc-rest/src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ Spring Framework 5 - Beginner ToGuru/spring-mvc-rest/src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -20,5 +24,21 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build()
                 .pathMapping("/");
+    }
+
+    private ApiInfo metaData() {
+
+        final Contact contact = new Contact("Fredrik Pedersen", "github.com/fredrikpedersen", "fredrikhp@gmail.com");
+
+        return new ApiInfo(
+                "Spring Framework Udemy Course",
+                "Spring Framework 5: From Beginner to Guru",
+                "1.0",
+                "Something something legal stuff",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LISENCE-2.0",
+                new ArrayList<>()
+        );
     }
 }
