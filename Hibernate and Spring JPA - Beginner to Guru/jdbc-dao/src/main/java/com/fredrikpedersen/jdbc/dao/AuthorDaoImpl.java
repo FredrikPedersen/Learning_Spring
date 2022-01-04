5708e9899c23e6f9f2a2c40379486fefd9cf59b0
Fredrik Pedersen <fredrikhp@gmail.com>
2022-01-04 17:47:53 +0100

    Completed Section 9: Part 67: Using Prepared Statements

--- Hibernate and Spring JPA - Beginner to Guru/jdbc-dao/src/main/java/com/fredrikpedersen/jdbc/dao/AuthorDaoImpl.java
+++ Hibernate and Spring JPA - Beginner to Guru/jdbc-dao/src/main/java/com/fredrikpedersen/jdbc/dao/AuthorDaoImpl.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 @Component
 @RequiredArgsConstructor
@@ -18,19 +18,24 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author getById(final Long id) {
+        final String query = "SELECT * FROM author WHERE id = ?";
 
         try (Connection connection = dataSource.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM author WHERE id = " + id)) {
+             PreparedStatement statement = connection.prepareStatement(query)) {
 
-            if (resultSet.next()) {
-                final Author author = new Author();
-                author.setId(id);
-                author.setFirstName(resultSet.getString("first_name"));
-                author.setLastName(resultSet.getString("last_name"));
+            statement.setLong(1, id);
 
-                return author;
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    final Author author = new Author();
+                    author.setId(id);
+                    author.setFirstName(resultSet.getString("first_name"));
+                    author.setLastName(resultSet.getString("last_name"));
+
+                    return author;
+                }
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
