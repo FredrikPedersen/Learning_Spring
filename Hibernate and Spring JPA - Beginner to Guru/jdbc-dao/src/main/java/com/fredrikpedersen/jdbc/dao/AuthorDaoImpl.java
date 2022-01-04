b75b8184db230f93a5ccd7645d3c09b9cc6f1301
Fredrik Pedersen <fredrikhp@gmail.com>
2022-01-04 17:32:18 +0100

    Completed Section 9: Part 64: Implement Get Author By Id

--- Hibernate and Spring JPA - Beginner to Guru/jdbc-dao/src/main/java/com/fredrikpedersen/jdbc/dao/AuthorDaoImpl.java
+++ Hibernate and Spring JPA - Beginner to Guru/jdbc-dao/src/main/java/com/fredrikpedersen/jdbc/dao/AuthorDaoImpl.java
@@ -1,13 +1,40 @@
 package com.fredrikpedersen.jdbc.dao;
 
 import com.fredrikpedersen.jdbc.domain.Author;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @Component
+@RequiredArgsConstructor
 public class AuthorDaoImpl implements AuthorDao {
+
+    private final DataSource dataSource;
 
     @Override
     public Author getById(final Long id) {
+
+        try (Connection connection = dataSource.getConnection()) {
+            final Statement statement = connection.createStatement();
+            final ResultSet resultSet = statement.executeQuery("SELECT * FROM author WHERE id = " + id);
+
+            if (resultSet.next()) {
+                final Author author = new Author();
+                author.setId(id);
+                author.setFirstName(resultSet.getString("first_name"));
+                author.setLastName(resultSet.getString("last_name"));
+
+                return author;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
