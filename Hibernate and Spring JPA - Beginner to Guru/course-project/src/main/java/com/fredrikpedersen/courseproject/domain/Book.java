bd297abb15c3f9783dc0369175f9045090ca0188
Fredrik Pedersen <fredrikhp@gmail.com>
2022-01-04 09:03:53 +0100

    Completed Section 6: Part 44: Initializing Data with Spring

--- Hibernate and Spring JPA - Beginner to Guru/course-project/src/main/java/com/fredrikpedersen/courseproject/domain/Book.java
+++ Hibernate and Spring JPA - Beginner to Guru/course-project/src/main/java/com/fredrikpedersen/courseproject/domain/Book.java
@@ -1,17 +1,23 @@
 package com.fredrikpedersen.courseproject.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
 @Entity
+@ToString
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class Book {
 
     @Id
-    @Column(columnDefinition = "serial")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -23,5 +29,18 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Book book = (Book) o;
+        return id != null && Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
